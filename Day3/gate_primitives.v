9c6fc2c0a5c5ce9e7a503cda11385fe3109071b0
Vennila200405 <vennilas200405@gmail.com>
2026-03-31 13:42:14 +0530

    Added gate primitives design and testbench

---
+++ Day3/gate_primitives.v
@@ -0,0 +1,12 @@
+module gate_primitives(a,b,y1,y2,y3,y4,y5,y6,y7,y8);
+input a,b;
+output y1,y2,y3,y4,y5,y6,y7,y8;
+and g1(y1,a,b);
+or g2(y2,a,b);
+nand g3(y3,a,b);
+nor g4(y4,a,b);
+xor g5(y5,a,b);
+xnor g6(y6,a,b);
+not g7(y7,a);
+buf g8(y8,a);
+endmodule
---
+++ Day3/gate_primitives_tb.v
@@ -0,0 +1,31 @@
+module gate_primitives_tb;
+reg a,b;
+wire y1,y2,y3,y4,y5,y6,y7,y8;
+gate_primitives dut(
+	.a(a), 
+	.b(b),
+	.y1(y1),
+	.y2(y2),
+	.y3(y3),
+	.y4(y4),
+	.y5(y5),
+	.y6(y6),
+	.y7(y7),
+	.y8(y8)
+);
+initial begin
+    a=1'b0;b=1'b0;#5
+    a=1'b0;b=1'b1;#5
+    a=1'b1;b=1'b0;#5
+    a=1'b1;b=1'b1;#5
+    a=1'b0;b=1'bx;#5
+    a=1'b1;b=1'bx;#5
+    a=1'b0;b=1'bz;#5
+    a=1'b1;b=1'bz;#5
+    a=1'bx;b=1'bz;#5 
+	$finish;
+end
+initial begin
+	$monitor("a=%b b=%b and_ab=%b or_ab=%b not_b=%b nand_ab=%b nor_ab=%b xor_ab=%b xnor_ab=%b buf_b=%b",a,b,y1,y2,y3,y4,y5,y6,y7,y8);
+end
+endmodule
